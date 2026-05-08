1d9ad1af302db4a59f8319e3d015eeebdd55731e
Apoorva-Khare <apkhare29@gmail.com>
2026-05-08 14:20:12 +0530

    setup

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-## End to end Machine LEarning Project
+## End to end Machine Learning Project
---
+++ setup.py
@@ -0,0 +1,31 @@
+# Building our application as a package itself -- so that other can install it and use it---setup.py use
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+#metadata indformation about the project
+
+#my function will return a list
+def get_requirements(file_path:str)->List[str]:
+    '''this function will returns the list of requirements'''
+    requirements= []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+setup(
+ name = "mlproject",
+ version ='0.0.1',
+ author = 'Apoorva',
+ author_email ='apkhare29@gmail.com',
+ packages=find_packages(),
+ #install_requires=['pandas','numpy','seaborn']
+ install_requires=get_requirements('requirements.txt')
+
+)
