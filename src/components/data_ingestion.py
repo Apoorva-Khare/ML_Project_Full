5ec2d8d2a328e3da0fc9cda1b861ea998d0bdfb5
Apoorva-Khare <apkhare29@gmail.com>
2026-05-13 19:50:45 +0530

    EDA and Problem Statement

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,12 @@
+import os 
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from  dataclasses import dataclass
+
+@dataclass   #data class decorator helpful for initialising data type
+class DataIngestionConfig:
+    train_data_path: str = os.path.join('artifacts',"train.csv")   #output will be saved in artifacts folder in train.csv
