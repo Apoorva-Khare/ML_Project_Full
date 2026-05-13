858a46f9e69d8c3508fb305fb8f41a0d2df483ec
Apoorva-Khare <apkhare29@gmail.com>
2026-05-13 20:32:15 +0530

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,43 @@ from  dataclasses import dataclass
 
 @dataclass   #data class decorator helpful for initialising data type
 class DataIngestionConfig:
+    #input given to dataingestion config and it knows where to store the data
     train_data_path: str = os.path.join('artifacts',"train.csv")   #output will be saved in artifacts folder in train.csv
     test_data_path: str = os.path.join('artifacts',"test.csv")
     raw_data_path: str = os.path.join('artifacts',"raw.csv")
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info('Entered the Data Ingestion method or components')
+        try:
+            df=pd.read_csv('notebook\data\stud.csv')
+            logging.info("Read the data as dataframe")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True) #will create artifact folder
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
+            logging.info('Train Test Split Initiated')
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+
+            #after train test split will save the data in artifact folder
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info("Ingestion of data is completed")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+#it will initiate/create artifact folder once it is executed
+if __name__=="__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
